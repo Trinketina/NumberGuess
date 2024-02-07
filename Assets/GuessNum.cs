73459667c1fba130e42950ff9e22504804c3225f
Tina <143371814+Trinketina@users.noreply.github.com>
2024-02-06 19:00:51 -0600

    Guess Game
    finished basics of guess the number game

---
+++ Assets/GuessNum.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GuessNum : MonoBehaviour
+{
+    [SerializeField] TMP_InputField guess;
+    [SerializeField] TMP_Text textbox;
+    [SerializeField] int defaultAttempts = 3;
+    int attempts;
+    int rand10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnReset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            OnGuess();
+        }
+    }
+
+    [ContextMenu("Guess Input")]
+    public void OnGuess()
+    {
+        if (guess == null || int.Parse(guess.text) > 10 || int.Parse(guess.text) <= 0)
+        {
+            textbox.text = "Enter a valid guess. >:c";
+        }
+        else if (int.Parse(guess.text) == rand10)
+        {
+            textbox.text = "YOU WON!!!";
+        }
+        else if (attempts > 2)
+        {
+            attempts--;
+            textbox.text = "Incorrect! You have " + attempts + " guesses remaining...";
+        }
+        else if (attempts > 1)
+        {
+            attempts--;
+            textbox.text = "Incorrect! You have " + attempts + " guess remaining...";
+        }
+        else
+        {
+            textbox.text = "YOU LOSE!!!";
+        }
+        EventSystem.current.SetSelectedGameObject(guess.gameObject);
+    }
+    public void OnReset()
+    {
+        rand10 = Random.Range(1, 11);
+        attempts = defaultAttempts;
+        //Debug.Log(rand10);
+        textbox.text = "I\'m thinking of a number between 1 and 10. You have " + attempts + " attempts to guess it...";
+    }
+}
